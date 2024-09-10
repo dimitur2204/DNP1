1e5678067c7231e953fdea3d15e2899ee582a0a4
Dimitar Nizamov <dimitur2204@gmail.com>
2024-09-10 19:27:43 +0200

    Add dummy data

--- Assignment2/CLI/Program.cs
+++ Assignment2/CLI/Program.cs
@@ -6,5 +6,13 @@ Console.WriteLine("Initializing...");
 ICommentRepository inMemoryCommentRepository = new CommentInMemoryRepository();
 IPostRepository inMemoryPostRepository = new PostInMemoryRepository();
 IUserRepository inMemoryUserRepository = new UserInMemoryRepository();
+inMemoryUserRepository.AddAsync(new Entities.User { Username = "admin", Password = "admin" }).Wait();
+inMemoryUserRepository.AddAsync(new Entities.User { Username = "user", Password = "user" }).Wait();
+inMemoryPostRepository.AddAsync(new Entities.Post { Title = "First post", Body = "First post content", WriterId = 1 })
+    .Wait();
+inMemoryPostRepository.AddAsync(new Entities.Post { Title = "Second post", Body = "Second post content", WriterId = 1 })
+    .Wait();
+inMemoryCommentRepository.AddAsync(new Entities.Comment { Body = "First comment", PostId = 1, WriterId = 1 }).Wait();
+inMemoryCommentRepository.AddAsync(new Entities.Comment { Body = "Second comment", PostId = 2, WriterId = 1 }).Wait();
 CliApp app = new CliApp(inMemoryCommentRepository, inMemoryPostRepository, inMemoryUserRepository);
 app.Run();
